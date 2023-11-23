c3f98da593d3ee8f4c6c7214524fbb1de3a3de2a
fenderson <freddenderson@gmail.com>
2023-11-23 10:20:01 -0600

    Explicacion auth controller

--- WebApiSalaVirtual/WebApiSalaVirtual/Controllers/v1/AuthController.cs
+++ WebApiSalaVirtual/WebApiSalaVirtual/Controllers/v1/AuthController.cs
@@ -38,18 +38,31 @@ namespace WebApiSalaVirtual.Controllers.v1
                 {
                     return StatusCode(StatusCodes.Status401Unauthorized, new { Auth = "Usuario sin Autorizacion" });
                 }
+
+                // Convierte la clave secreta en un array de bytes utilizando codificación ASCII
                 var keyBytes = Encoding.ASCII.GetBytes(secretKey);
+
+                // Crea un objeto ClaimsIdentity que contendrá las reclamaciones asociadas al usuario
                 var claims = new ClaimsIdentity();
+
+                // Agrega una reclamación al objeto ClaimsIdentity (en este caso, el nombre del usuario)
                 claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, request.Nombre));
+
+                // Crea un descriptor de token de seguridad con la información necesaria
                 var tokenDescriptor = new SecurityTokenDescriptor
                 {
-                    Subject = claims,
-                    Expires = DateTime.UtcNow.AddMinutes(240),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                    Subject = claims, // Asigna el objeto ClaimsIdentity al descriptor
+                    Expires = DateTime.UtcNow.AddMinutes(480), // Establece la fecha de expiración del token
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature) // Asigna las credenciales de firma usando la clave secreta
                 };
 
+                // Crea un manejador de tokens JWT
                 var tokenHandler = new JwtSecurityTokenHandler();
+
+                // Crea un token JWT utilizando el descriptor de token de seguridad
                 var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+                // Convierte el token JWT en una cadena
                 string tokencreado = tokenHandler.WriteToken(tokenConfig);
 
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = new { user, token = tokencreado } });
@@ -68,14 +81,11 @@ namespace WebApiSalaVirtual.Controllers.v1
         {
             var user = null as VwRolesRutas;
             user = _context.VwRolesRutas.FirstOrDefault(obj => obj.Rol == request.Rol && obj.NombreRuta == request.Ruta);
-
             if (user == null)
             {
                 return StatusCode(StatusCodes.Status401Unauthorized, new { mensaje = "Usuario sin Autorizacion de la Ruta", response = new { auth = false } });
             }
-
             return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = new { auth = true } });
         }
-
     }
 }
